d196d0fa112d29edcae2ec53ad44c80c7b085d15
Aran <a.farshy@gmail.com>
2022-12-29 00:41:52 +0100

    SDL init & Controllerinputs in Hintergrund werden erkannt

--- JoyToTouch++/mainApp.cpp
+++ JoyToTouch++/mainApp.cpp
@@ -1,12 +1,69 @@
 #include "mainApp.h"
 //#define WinMain main
 
-wxIMPLEMENT_APP_NO_MAIN(mainApp);	// benötigt in main wxEntry
+wxIMPLEMENT_APP_NO_MAIN(mainApp);	// benötigt in main 
+
+//SDL_WarpMouseGlobal(100, 100);	// verschiebt MouseZeiger
+//SDL_GetGlobalMouseState(&x, &y);	// Nimmt Mousezeiger Position
 
 int main(int argc, char** argv) {		// Subsystem Console in Linker/System aktiviert
 	//std::cout << "Test" << std::endl;
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0)	// Init SDL für Controller
+	{
+		fprintf(stderr, "Couldn't initialize SDL: %s\n", SDL_GetError());
+		exit(1);
+	}
 
-	return wxEntry(argc, argv);
+	printf("%i joysticks were found.\n\n", SDL_NumJoysticks());
+
+	for (int i = 0; i < SDL_NumJoysticks(); i++)
+	{
+		//SDL_Joystick* controller = SDL_JoystickNameForIndex
+
+		if (SDL_JoystickNameForIndex(i) == NULL) {
+			printf("Fehler beim Öffnen von Controller %d\n", i);
+		}
+		else {
+			//printf("    Index: %d - Name: %s\n", i, SDL_JoystickName(SDL_JoystickOpen(i)));	// Bekomme Controller Instanz und erhalte den Namen daraus
+			printf("    Index: %d - Name: %s\n", i, SDL_JoystickNameForIndex(i));	// Besser als zu Öffnen
+		}
+	}
+
+	//SDL_Window* gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 320, SDL_WINDOW_SHOWN); /* I don't want this window! */
+
+	// ControllerInput soll auch im Hintergrund möglich sein
+	SDL_SetHint(SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS, "1");
+
+	// Nutze EventHandler und öffne Controller
+	SDL_JoystickEventState(SDL_ENABLE);
+	//SDL_Joystick* controller = SDL_JoystickOpen(0);	// wähle Controller 0
+	SDL_GameController* controller = SDL_GameControllerOpen(0);	// wähle Controller 0
+
+	if (controller == NULL) {
+		printf("Controller Opening Fail\n");
+	}
+	else {
+		SDL_Event event;
+
+		while (1) {
+			while (SDL_PollEvent(&event)) {
+
+				printf("%d\n", event.type);
+
+				/*switch (event.type) {
+				case SDL_KEYDOWN:
+					printf("Reached\n");
+					break;
+				case SDL_CONTROLLERBUTTONDOWN:
+					printf("BDown\n");
+					break;
+				}*/
+			}
+		}
+	}
+
+	//return wxEntry(argc, argv);
+	return 0;
 }
 
 bool mainApp::OnInit() {
--- JoyToTouch++/mainApp.h
+++ JoyToTouch++/mainApp.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <wx/wx.h>
+#include <SDL.h>
 #include "fenster.h"
 class mainApp : public wxApp
 {
